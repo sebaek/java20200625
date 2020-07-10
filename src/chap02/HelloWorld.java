e1fd08b7aaa705d8c671d1b3ffc3d57ce52e4ff7
sebaek <sebaek@gmail.com>
2020-07-10 09:34:19 +0900

    ref type

---
+++ src/chap02/HelloWorld.java
@@ -0,0 +1,35 @@
+package chap02;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		int i;
+		i = 40;
+		
+		int j = 3;
+		
+		int z = i + j;
+		System.out.println(z);
+		
+		if (z > 3) {
+			System.out.println("z 는 3보다 크다.");
+		} else {
+			System.out.println("z 는 3보다 크지 않다.");
+		}
+		
+		for (int k = 0; k < 5; k++) {
+			System.out.println(k);
+		}
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ src/chap05/RefType.java
@@ -0,0 +1,25 @@
+package chap05;
+
+public class RefType {
+	public static void main(String[] args) {
+		// int, short, byte, long
+		// boolean, char
+		// float, double
+		
+		String s = "hello";
+		int[] arr = new int[3];
+		int i = 3;
+
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
