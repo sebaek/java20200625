aa438c0cb9d4e5b8907b8606570916eee23292f9
sebaek <sebaek@gmail.com>
2020-07-24 11:10:30 +0900

    solution

--- src/codingbat/SolutionArray3.java
+++ src/codingbat/SolutionArray3.java
@@ -141,4 +141,16 @@ public class SolutionArray3 {
 
 	}
 
+	public int[] squareUp(int n) {
+		int[] res = new int[n * n];
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				res[n * i - j] = j;
+			}
+		}
+
+		return res;
+	}
+
 }
