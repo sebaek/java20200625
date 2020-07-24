b742568747cbdad9057f6a943d047b53c9dbc5dd
sebaek <sebaek@gmail.com>
2020-07-24 14:08:12 +0900

    array3

--- src/codingbat/SolutionArray3.java
+++ src/codingbat/SolutionArray3.java
@@ -153,4 +153,64 @@ public class SolutionArray3 {
 		return res;
 	}
 
+	public int[] seriesUp(int n) {
+		int len = (1 + n) * n / 2;
+		int[] res = new int[len];
+
+		for (int i = 1, k = 0; i <= n; i++) {
+			for (int j = 1; j <= i; j++, k++) {
+				res[k] = j;
+			}
+		}
+
+		return res;
+	}
+
+	public int maxMirror(int[] nums) {
+		int max = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = nums.length - 1; j >= 0; j--) {
+				max = Math.max(max, check(nums, i, j));
+			}
+		}
+
+		return max;
+	}
+
+	private int check(int[] nums, int i, int j) {
+		int res = 0;
+		while (i < nums.length && j >= 0 && nums[i] == nums[j]) {
+			i++;
+			j--;
+			res++;
+		}
+
+		return res;
+	}
+
+	public int countClumps(int[] nums) {
+		if (nums.length == 0) {
+			return 0;
+		}
+
+		int res = 0;
+		int i = 0;
+		int prev = nums[i] - 1;
+
+		while (i < nums.length) {
+			if (nums[i] == prev) {
+				res++;
+				while (i < nums.length && nums[i] == prev) {
+					i++;
+				}
+			} else {
+				prev = nums[i];
+				i++;
+			}
+		}
+
+		return res;
+	}
+
 }
