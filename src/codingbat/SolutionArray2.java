b8ae927ed573e925636097cb2f718a584b0bcb0c
sebaek <sebaek@gmail.com>
2020-08-21 16:19:21 +0900

    array2

--- src/codingbat/SolutionArray2.java
+++ src/codingbat/SolutionArray2.java
@@ -169,4 +169,136 @@ public class SolutionArray2 {
 
 	}
 
+	public boolean no14(int[] nums) {
+		boolean e1 = false;
+		boolean e4 = false;
+
+		for (int n : nums) {
+			if (n == 1) {
+				e1 = true;
+			}
+
+			if (n == 4) {
+				e4 = true;
+			}
+		}
+
+		return !e1 || !e4;
+	}
+
+	public boolean isEverywhere(int[] nums, int val) {
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] != val && nums[i + 1] != val) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public boolean either24(int[] nums) {
+		boolean e2 = false;
+		boolean e4 = false;
+
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == nums[i + 1]) {
+				if (nums[i] == 2) {
+					e2 = true;
+				}
+
+				if (nums[i] == 4) {
+					e4 = true;
+				}
+			}
+		}
+
+		return e2 ^ e4;
+	}
+
+	public int matchUp(int[] nums1, int[] nums2) {
+		int cnt = 0;
+
+		for (int i = 0; i < nums1.length; i++) {
+			int diff = Math.abs(nums1[i] - nums2[i]);
+			if (diff == 1 || diff == 2) {
+				cnt++;
+			}
+		}
+
+		return cnt;
+	}
+
+	public boolean has77(int[] nums) {
+
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == 7) {
+				int next1 = 0; // nums[i+1];
+				int next2 = 0; // nums[i+2];
+
+				if (i + 1 < nums.length) {
+					next1 = nums[i + 1];
+				}
+
+				if (i + 2 < nums.length) {
+					next2 = nums[i + 2];
+				}
+
+				if (next1 == 7 || next2 == 7) {
+					return true;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	public boolean has12(int[] nums) {
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 1) {
+				for (int j = i + 1; j < nums.length; j++) {
+					if (nums[j] == 2) {
+						return true;
+					}
+				}
+
+				break;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean modThree(int[] nums) {
+
+		for (int i = 1; i < nums.length - 1; i++) {
+			int rCenter = nums[i] % 2;
+			int rLeft = nums[i - 1] % 2;
+			int rRight = nums[i + 1] % 2;
+
+			int sum = rCenter + rLeft + rRight;
+
+			if (sum == 3 || sum == 0) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public boolean haveThree(int[] nums) {
+		int cnt = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 3) {
+				cnt++;
+
+				if (i + 1 < nums.length && nums[i + 1] == 3) {
+					return false;
+				}
+			}
+		}
+
+		return cnt == 3;
+	}
+
 }
